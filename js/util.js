45de2afbb76d8aa00ae18c5a73df3917e33850f7
typable <typable@prototype-studio.de>
2021-03-10 11:40:13 +0100

    Updated html function

--- js/util.js
+++ js/util.js
@@ -1,15 +1,8 @@
 export function html(func) {
-	return {
-		build: function(args) {
-			let template = document.createElement('template');
-			template.innerHTML = func(args);
-			let fragment = template.content.cloneNode(true);
-			return {
-				element: fragment.firstElementChild,
-				render: function(parent) {
-					parent.appendChild(fragment);
-				}
-			};
-		}
+	return function(props) {
+		const template = document.createElement('template');
+		template.innerHTML = func(props);
+		const fragment = template.content.cloneNode(true);
+		return fragment.firstElementChild;
 	};
 }
