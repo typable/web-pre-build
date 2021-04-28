64163bee8f83fd9b4e39b19e8d4f7281eb64bf66
typable <typable.dev@gmail.com>
2021-04-28 09:55:37 +0200

    Merged less files from git-typable

--- js/util.js
+++
@@ -1,8 +0,0 @@
-export function html(func) {
-	return function(props) {
-		const template = document.createElement('template');
-		template.innerHTML = func(props);
-		const fragment = template.content.cloneNode(true);
-		return fragment.firstElementChild;
-	};
-}
